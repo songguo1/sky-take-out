4fe7085030cf5fd9a4b3c69b6b8a623827799fa1
songguo <202221165028@njtech.edu.cn>
2024-09-04 23:34:09 +0800

    购物车功能bug修复

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -17,7 +17,7 @@ public interface ShoppingCartMapper {
     void updateNumberById(ShoppingCart shoppingCart);
 
     @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) "+
-    "values (#{name},#{image},#{user_id},#{dish_id},#{setmeal_id},#{dish_flavor},#{number},#{amount},#{ create_time})")
+    "values (#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{ createTime})")
     void insert(ShoppingCart shoppingCart);
 
     @Delete("delete from shopping_cart where user_id =#{userId}")
